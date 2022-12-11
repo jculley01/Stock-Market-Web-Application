7b8e36058ebc4cccee1404f6a28789ea08eb15ae
kimery9 <114025961+kimery9@users.noreply.github.com>
2022-12-11 18:16:21 -0500

    FINAL Aggregate.jsx

--- Aggregate.jsx
+++ Aggregate.jsx
@@ -5,38 +5,44 @@ import Chart from 'react-apexcharts';
 
 
 async function getStocks(query,current,day){
+    //call the polygon.io aggregate section, passing the date parameters and query input.
     const apiUrl=`https://api.polygon.io/v2/aggs/ticker/${query.query.toString()}/range/1/day/${current.getFullYear()-1}-${current.getMonth()+1}-${day}/${current.getFullYear()}-${current.getMonth()+1}-${day}?adjusted=true&sort=asc&limit=500&apiKey=QVcqDoOgvanAJx0Tjpj01BtguV0OLYoA`;
     const response=await fetch(apiUrl);
+    //return object
     return response.json()
 }
 
 
 async function getReal(query) {
-    const finnhub=`https:finnhub.io/api/v1/quote?symbol=${query.query.toString()}&token=ce7ok3qad3i4pjr4oj10ce7ok3qad3i4pjr4oj1g`
+    //call the finnhub API quote section with inputted query
+    const finnhub=`https:finnhub.io/api/v1/quote?symbol=${query.query.toString()}&token=ce7ok3qad3i4pjr4oj10ce7ok3qad3i4pjr4oj1g` 
     const response=await fetch(finnhub);
+    //return object
     return response.json()
 }
 
 async function getdetails(query){
+    //call the polygon.io details section with query input
     const apiUrl=`https://api.polygon.io/v3/reference/tickers/${query.query.toString()}?apiKey=quZjqwprpqMbql20ikgeHo7Sbfh_4v8H`
     const response=await fetch(apiUrl);
+    //return object
     return response.json()
 }
 
 function Aggregate (query){
-    const current = new Date();
+    const current = new Date();//store the current system date withing current.
     let day;
-    if(current.getDate()<=10){
+    if(current.getDate()<=10){//convert day to be correct format needed in API call 9->09
         day=('0'+(current.getDate()-1))
     }
     else{
         day=current.getDate()-1;
     }
-    console.log(query.query.toString());
+    
     const [series, setSeries]=useState( [{
         data: []
     }]);
-    // const [tablevals, settablevals]=useState(-1);
+   //initialize needed variables as useStates
     const [price, setPrice]=useState(0);
     const [pastPrice, setPastPrice]=useState(0);
     const [priceTime,setPriceTime]=useState(null);
@@ -45,20 +51,17 @@ function Aggregate (query){
     const [descript,setdescript]=useState('')
 
 
-
+//getStocks useEffect calls the function and returns object in promise variable data, with dependency on query
     useEffect(()=>{
             getStocks(query,current,day)
                 .then((data) => {
-                    console.log(data);
                     const length = data.results.length;
                     const historical = [];
-                    for (let i = 0; i < length; i++) {
+                    for (let i = 0; i < length; i++) {//manipulate the data into array to accomidated apexcharts documentation
                         const tix = data.results[i];
                         historical[i] = [tix.t, tix.o, tix.h, tix.l, tix.c];
                     }
                     
-
-
                     setSeries([{
                         data: historical
                     }])
@@ -66,10 +69,10 @@ function Aggregate (query){
                 })
 
     },[query])
-
+//create chart according to apexchart documentation
     const chart= {
         options: {
-            chart: {
+            chart: {//style chart
                 foreColor: '#ffffff',
                 type: 'candlestick',
                 height: 500
@@ -88,39 +91,35 @@ function Aggregate (query){
             }
         },
     };
-
+//getdetails useEffect calls the function and returns object in promise variable detail, with dependency on query. The data is then indexed to store company logo url, main url, and description
     useEffect(()=>{
         getdetails(query)
             .then((detail) => {
                 const deets = detail.results;
-                console.log(deets)
                 setlogo(deets.branding.logo_url + '?apiKey=quZjqwprpqMbql20ikgeHo7Sbfh_4v8H')
                 setmainurl(deets.homepage_url)
                 setdescript(deets.description)
-                console.log(descript);
 
             })
     },[query])
+    //The useEffect has an imbedded async function getlive in order to handle the refresh of the live ticker, dependency on query and price
     useEffect(()=>{
         let timeoutID;
         async function getlive() {
-            const livetix=await getReal(query)
+            const livetix=await getReal(query)//call the function and store the obkect in livetix
             const liveprice=livetix.c
-            console.log(liveprice)
             setPastPrice(price);
              setPrice(liveprice);
            timeoutID=setTimeout(getlive,5000);
         }
-        timeoutID=setTimeout(getlive,5000);
+        timeoutID=setTimeout(getlive,5000);//refresh the call every 5 seconds
         return()=>{
-            clearTimeout(timeoutID);
+            clearTimeout(timeoutID);//clear refresh
         }
 
     },[price,query])
-    // console.log('Price'+price);
-    // console.log('Past'+pastPrice);
 
-
+//assign correct css tag to the liveticker display depending on whether the price went up, down, or stayed the same
 const direction= useMemo(()=>pastPrice<price?'up':pastPrice>price?'down':'',[pastPrice,price]);
 
 
